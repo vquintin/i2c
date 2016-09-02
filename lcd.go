b3226ce40f7a069d3de5a8d920c2b6a95e9ee648
Virgile Quintin <virgile.quintin@gmail.com>
2016-09-03 01:32:27 +0200

    WIP

--- lcd.go
+++ lcd.go
@@ -77,7 +77,7 @@ func (lcd *Lcd) enable(data byte) {
 	} else {
 		data = pinInterpret(lcd.backlight, data, false)
 	}
-	log.Print(data)
+	log.Printf("%#x", data)
 	lcd.i2c.WriteByte(data)
 	time.Sleep(500 * time.Microsecond)
 	lcd.i2c.WriteByte(pinInterpret(lcd.en, data, true))
